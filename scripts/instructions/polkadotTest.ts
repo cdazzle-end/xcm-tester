7b7bee07a75aee26834f04c4fb049b88f3bda00f
dazzlec123 <dazzlec123@gmail.com>
2024-08-10 02:27:15 +0000

    Aug 9, 2024, 10:27 PM

--- scripts/instructions/polkadotTest.ts
+++ scripts/instructions/polkadotTest.ts
@@ -1120,7 +1120,7 @@ async function testParaspellReworked(){
     let signer = await getSigner(chopsticks, eth);
 
     let startNodeBalance = await getBalanceFromId(startParaId, relay, chopsticks, startApi, startNodeAsset, startNode, signer.address)
-    let destNodeBalance = await getBalanceFromId(startParaId, relay, chopsticks, startApi, startNodeAsset, startNode, signer.address)
+    let destNodeBalance = await getBalanceFromId(destParaId, relay, chopsticks, startApi, startNodeAsset, startNode, signer.address)
 
     console.log(`Balance for ${assetSymbol} on ${startNode} for ${signer.address}: ${JSON.stringify(startNodeBalance)}`)
     console.log(`Balance for ${assetSymbol} on ${destNode} for ${signer.address}: ${JSON.stringify(destNodeBalance)}`)
